f5a879ab7d74d31714d7151fc0d5bd98f46715bb
tioloverain <apo_pro2000@yahoo.com>
2012-06-06 06:27:13 +0700

    update

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 require 'rubygems'
 require 'Spreadsheet'
 #require 'pdfkit'
-Date::MONTHNAMES=[nil,"tháng 1","tháng 2","tháng 3","tháng 4","tháng 5","tháng 6","tháng 7","tháng 8","tháng 9","tháng 10","tháng 11","tháng 12"]
+#Date::MONTHNAMES=[nil,"tháng 1","tháng 2","tháng 3","tháng 4","tháng 5","tháng 6","tháng 7","tháng 8","tháng 9","tháng 10","tháng 11","tháng 12"]
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
